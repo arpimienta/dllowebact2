bfd3f1a9076cbea361769944936a1af81a8e330e
Andres Pimienta <dtarazona@uniguajira.edu.co>
2021-09-23 00:00:48 -0500

    agregado del ts

---
+++ js/del.js
@@ -0,0 +1,22 @@
+"use strict";
+//Delincuente 1
+const delincuente1 = {
+    id: 444456667,
+    nombreDel: "Jairo Mendoza",
+    telDel: 1234567
+};
+console.log(`Nombre del Delincuente:${delincuente1.nombreDel}, id del delincuente:${delincuente1.id} y telefono del delincuente:${delincuente1.telDel}`);
+//Delincuente 2
+const delincuente2 = {
+    id: 777788888,
+    nombreDel: "Brayan Lopez",
+    telDel: 30000546
+};
+console.log(`Nombre del Delincuente:${delincuente2.nombreDel}, id del delincuente:${delincuente2.id} y telefono del delincuente:${delincuente2.telDel}`);
+//Delincuente 3
+const delincuente3 = {
+    id: 44497223813,
+    nombreDel: "Oscar Gomez",
+    telDel: 312567798
+};
+console.log(`Nombre del Delincuente:${delincuente3.nombreDel}, id del delincuente:${delincuente3.id} y telefono del delincuente:${delincuente3.telDel}`);
